8ced30ad4ccd1a0d95d7c56d72e5f87080c3e1ef
Nikita Nikitchenko <nikitos13.n@gmail.com>
2025-04-13 00:05:46 +0400

    fix all

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,16 +1,18 @@
+import sys
+import os
+
+# Добавляем путь к корню проекта
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
-from web.app import app
+from app import app
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-
 def test_hello(client):
     response = client.get('/hello')
     assert response.status_code == 200
     assert response.data == b'Hello, World!'
-
-def test_true():
-    assert 1 == 1
